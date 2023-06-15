84c35fb271d00c38e73858cc509c0a08a21e6848
StephanJ98 <diego.jeandon@gmail.com>
2023-06-15 16:00:44 +0200

    :pencil: UI modification

--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,13 @@ import { KeyboardEvent, useState } from 'react'
 export default function Home() {
   const { logged, setLogged } = useLog()
   const [value, setValue] = useState('')
+  const [bgBtn, setBgBtn] = useState<boolean>(true)
 
   const checkPass = (): boolean => {
     if (value === process.env.NEXT_PUBLIC_PASS) {
       setLogged()
+    } else {
+      setBgBtn(false)
     }
     return true
   }
@@ -35,11 +38,11 @@ export default function Home() {
           <>
             <Header />
             <div className='flex flex-col h-[30vh] w-full items-center justify-center'>
-              <div className='bg-white p-5 shadow-md rounded-xl flex items-center justify-center w-[90vw] md:w-auto'>
+              <div className={`${bgBtn ? 'bg-white' : 'bg-red-400'} p-5 shadow-md rounded-xl flex items-center justify-center w-[90vw] md:w-auto`}>
                 <input
                   type="password"
                   placeholder='Password'
-                  className='focus:outline-none w-full rounded-md'
+                  className='focus:outline-none w-full rounded-md bg-inherit'
                   value={value}
                   onChange={(e) => setValue(e.target.value)}
                   onKeyDown={(e) => handleSubmit(e)}
